848bc421d154456643995613cdb95cd43cf8c115
Szabboy17 <129592254+Szabboy17@users.noreply.github.com>
2023-09-24 14:47:22 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+function Clickep1(){
+    if(Click){
+        window.location = "ep1.html"
+    }
+}
+
+function Clickep2(){
+    if(Click){
+        window.location = "ep2.html"
+    }
+}
+
+function Clickep3(){
+    if(Click){
+        window.location = "ep3.html"
+    }
+}
+
+function Clickep4(){
+    if(Click){
+        window.location = "ep4.html"
+    }
+}
+
+function Clickep5(){
+    if(Click){
+        window.location = "ep5.html"
+    }
+}
+
+function Clickep6(){
+    if(Click){
+        window.location = "ep6.html"
+    }
+};
+
+function Clickback2main(){
+    if(Click){
+        window.location = "sw.html"
+    }
+}
